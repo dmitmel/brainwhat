b4bf3d13d8be50180135a3c8bfa69010b9cfa746
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2018-07-29 13:35:21 +0300

    add Display implementation for Instruction

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialOrd, PartialEq, Copy, Clone)]
 pub enum Instruction {
   Move(isize),
@@ -6,4 +8,30 @@ pub enum Instruction {
   Read,
   JumpIfZero(usize),
   JumpIfNonZero(usize),
+}
+
+impl fmt::Display for Instruction {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    use Instruction::*;
+
+    write!(
+      f,
+      "{}",
+      match self {
+        Move(n) if *n > 0 => ">".repeat(*n as usize),
+        Move(n) if *n < 0 => "<".repeat(n.abs() as usize),
+
+        Add(n) if *n > 0 => "+".repeat(*n as usize),
+        Add(n) if *n < 0 => "-".repeat(n.abs() as usize),
+
+        Print => ".".to_owned(),
+        Read => ",".to_owned(),
+
+        JumpIfZero(_) => "[".to_owned(),
+        JumpIfNonZero(_) => "]".to_owned(),
+
+        _ => String::default(),
+      }
+    )
+  }
 }
